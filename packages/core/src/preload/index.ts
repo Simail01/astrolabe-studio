051e2a38882dde726539905e0d945d0570c0d6ba
绯惜 <1085520846@qq.com>
2026-05-21 23:09:09 +0800

    feat(core): add preload script with contextBridge API

---
+++ packages/core/src/preload/index.ts
@@ -0,0 +1,21 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+export interface AstrolabeAPI {
+  invoke: (channel: string, ...args: unknown[]) => Promise<unknown>;
+  on: (channel: string, callback: (...args: unknown[]) => void) => () => void;
+  once: (channel: string, callback: (...args: unknown[]) => void) => void;
+}
+
+const api: AstrolabeAPI = {
+  invoke: (channel: string, ...args: unknown[]) => ipcRenderer.invoke(channel, ...args),
+  on: (channel: string, callback: (...args: unknown[]) => void) => {
+    const listener = (_event: Electron.IpcRendererEvent, ...args: unknown[]) => callback(...args);
+    ipcRenderer.on(channel, listener);
+    return () => ipcRenderer.removeListener(channel, listener);
+  },
+  once: (channel: string, callback: (...args: unknown[]) => void) => {
+    ipcRenderer.once(channel, (_event, ...args) => callback(...args));
+  },
+};
+
+contextBridge.exposeInMainWorld('astrolabe', api);
